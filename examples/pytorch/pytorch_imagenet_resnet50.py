e71182bab1666dec4a0fc3180f30741463f54c57
duy.le281 <duy.le281@hcmut.edu.vn>
2023-04-18 14:05:55 +0700

    change data of  examples/pytorch/pytorch_imagenet_resnet50.py from imagenet to imagnet-mini

--- examples/pytorch/pytorch_imagenet_resnet50.py
+++ examples/pytorch/pytorch_imagenet_resnet50.py
@@ -15,9 +15,9 @@ from tqdm import tqdm
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Example',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--train-dir', default=os.path.expanduser('~/imagenet/train'),
+parser.add_argument('--train-dir', default=os.path.expanduser('~/imagenetmini-1000/imagenet-mini/train'),
                     help='path to training data')
-parser.add_argument('--val-dir', default=os.path.expanduser('~/imagenet/validation'),
+parser.add_argument('--val-dir', default=os.path.expanduser('~/imagenetmini-1000/imagenet-mini/validation'),
                     help='path to validation data')
 parser.add_argument('--log-dir', default='./logs',
                     help='tensorboard log directory')
